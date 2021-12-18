f222e0e1fc215f5cec7f54f881fc59b33d2c6e77
ZhansauleT <jansaule97@mail.ru>
2021-12-19 01:53:43 +0600

    create: form + validation, video interactive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const categoriesSlider = new Swiper('.categories-slider', {
+$(document).ready(function () {
+  var categoriesSlider = new Swiper('.categories-slider', {
   // Optional parameters
   loop: false,
 
@@ -14,7 +15,7 @@ const categoriesSlider = new Swiper('.categories-slider', {
 
 });
 
-const unreleasedSlider = new Swiper('.unreleased-slider', {
+var unreleasedSlider = new Swiper('.unreleased-slider', {
   // Optional parameters
   loop: false,
 
@@ -27,4 +28,169 @@ const unreleasedSlider = new Swiper('.unreleased-slider', {
     nextEl: '.unreleased-slider__button--next',
     prevEl: '.unreleased-slider__button--prev',
   },
+});
+
+//play the video
+
+$('.story-main__button').click(function(){
+  if(($(".story-main__video").get(0).paused) === true){
+    $(".story-main__video").get(0).play();
+    $('.story-main__button').hide();
+  }
+});
+
+$('.story-main__video').click(function(){
+  if(($(".story-main__video").get(0).paused) === false){
+    $(".story-main__video").get(0).pause();
+    $('.story-main__button').show();
+  }
+});
+
+//video 1
+
+$('.story-secondary__button--first').click(function(){
+  if(($(".story-secondary__video--first").get(0).paused) === true){
+    $(".story-secondary__video--first").get(0).play();
+    $('.story-secondary__button--first').hide();
+  }
+});
+
+$('.story-secondary__video--first').click(function(){
+  if(($(".story-secondary__video--first").get(0).paused) === false){
+    $(".story-secondary__video--first").get(0).pause();
+    $('.story-secondary__button--first').show();
+  }
+});
+
+//video 2
+$('.story-secondary__button--second').click(function(){
+  if(($(".story-secondary__video--second").get(0).paused) === true){
+    $(".story-secondary__video--second").get(0).play();
+    $('.story-secondary__button--second').hide();
+  }
+});
+
+$('.story-secondary__video--second').click(function(){
+  if(($(".story-secondary__video--second").get(0).paused) === false){
+    $(".story-secondary__video--second").get(0).pause();
+    $('.story-secondary__button--second').show();
+  }
+});
+
+
+//video 3
+$('.story-secondary__button--third').click(function(){
+  if(($(".story-secondary__video--third").get(0).paused) === true){
+    $(".story-secondary__video--third").get(0).play();
+    $('.story-secondary__button--third').hide();
+  }
+});
+
+$('.story-secondary__video--third').click(function(){
+  if(($(".story-secondary__video--third").get(0).paused) === false){
+    $(".story-secondary__video--third").get(0).pause();
+    $('.story-secondary__button--third').show();
+  }
+});
+
+
+
+// $('.story-secondary__button--first').click(function(){
+//   $(".story-secondary__video").get(0).paused ? $(".story-secondary__video").get(0).play() : $(".story-secondary__video").get(0).pause();
+// });
+
+// $('.story-secondary__button--second').click(function(){
+//   $(".story-secondary__video").get(1).paused ? $(".story-secondary__video").get(1).play() : $(".story-secondary__video").get(1).pause();
+// });
+
+// $('.story-secondary__button--third').click(function(){
+//   $(".story-secondary__video").get(2).paused ? $(".story-secondary__video").get(2).play() : $(".story-secondary__video").get(2).pause();
+// });
+
+
+var menuButton = $(".menu-button");
+menuButton.on('click', function(){
+  $(".navbar-bottom").toggleClass('navbar-bottom--visible');
+});
+
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $(".modal__close");
+var mainPageButton = $(".gratitude__button");
+
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+//go to the main page button
+mainPageButton.on('click', goToMainPage);
+
+//to go to the main page
+function goToMainPage(event){
+  event.preventDefault();
+  window.location.replace("index.html");
+}
+
+//to close the modal window when esc is pressed
+//$(document).keydown(closeModalEsc);
+
+// var modal = $(".modal");
+
+// // When the user clicks anywhere outside of the modal, close it
+// $(window).click(function(event) {
+//   if (event.target == modal) {
+//     closeModal(event);
+//   }
+// });
+
+function openModal(){
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+  $('body').css('overflow','hidden');
+}
+
+function closeModal(event){
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+  $('body').css('overflow','auto');
+}
+
+  //to close the modal window by pressing Esc
+  function closeModalEsc(event){
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    if(event.keyCode === 27){
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+    } 
+  }
+
+  //Form validation
+  $(".form").each(function() {
+    $(this).validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2,
+      },
+    },
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "Your name must contain more than 2 characters",
+      },
+      email: {
+        required: "We need your email to contact you",
+        email: "The format: name@domain.com",
+      },
+      phone: {
+        required: "Phone number is required in format: +7(777)777-77-77",
+        minlength: "Please, enter a valid phone number",
+      },
+    }
+  });
+  })                               
 });
